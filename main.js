4d2654599412f2b1c5a421326095f81be2c4bcc8
e-Pixel <ipixelsun@gmail.com>
2021-08-24 21:26:51 -0700

    modified links

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var testToday = new Date();
 var tstDay = testToday.getDay();
-console.log(tstDay);
 
 function messageOnlyWeekends() {
   var today = new Date();
@@ -14,8 +13,38 @@ function messageOnlyWeekends() {
   }
 }
 
+const courseId = 70356;
+
+// asume que existe una variable llamada
+// courseId con la ID del curso que se quiere
+const url = `https://cursos.tecmilenio.mx/api/v1/calendar_events?context_codes[]=course_${courseId}`;
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", url);
+
+
+const token = "14121~3TX55L1q034g2VM5Y2rqunwVqTQdYTaQscAWXW2xhLfqew6783K6XHGVO9YAI3j0"; 
+// Put it as private DO NOT SHARE 
+xhr.setRequestHeader("Authorization", `Bearer ${token}`);
+
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+      const calendarEvent = JSON.parse(xhr.responseText);
+      // esto de arriba es asumiendo que no falla.
+      // no se bien como manejar errores aun
+      // puedes acceder al codigo de respuesta con xhr.status
+
+      // si todo va bien, tienes que sacar la propiedad description
+      // del objeto calendarEvent. ahí va a estar el link de zoom.
+      // tienes que ingeniar alguna manera de aislarlo
+      console.log(calendarEvent.description)
+   }};
+
+xhr.send();
+// console.log(xhr.status)
+
 messageOnlyWeekends();
-console.log(testToday.getMonth());
+// console.log(testToday.getMonth());
 /*
     0 - Sunday
     1 - Monday
