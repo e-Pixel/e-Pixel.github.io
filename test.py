3f46b1d21bd388c74469124fd9486c79f090a90f
e-Pixel <ipixelsun@gmail.com>
2022-01-28 08:46:23 -0700

    temporary fix art link[

--- test.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-f = int(input())
-
-def multiply(number, finalRange):
-  for i in range(finalRange + 1):
-    print("{0} * {1} = {2} ".format(number, i, number * i))
-
-multiply(n, f)
